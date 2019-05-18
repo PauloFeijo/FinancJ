27f3e978aa3673651ce28243c27c369a3dc46120
PauloFeijo <paulorikrdo.89@gmail.com>
2019-05-18 13:16:17 -0300

    CRUD Conta

--- server/FinancJ/src/main/java/com/feijo/financj/resources/ContaResource.java
+++ server/FinancJ/src/main/java/com/feijo/financj/resources/ContaResource.java
@@ -1,14 +1,18 @@
 package com.feijo.financj.resources;
 
-import java.util.ArrayList;
+import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.feijo.financj.domain.Conta;
 import com.feijo.financj.services.ContaService;
@@ -27,5 +31,43 @@ public class ContaResource {
 		
 		return ResponseEntity.ok().body(conta);
 	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Conta>> findAll() {
+
+		List<Conta>  lista = serv.findAll();
+
+		return ResponseEntity.ok().body(lista);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Conta obj) {
+		
+		obj = serv.insert(obj);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
+				path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody Conta obj, @PathVariable Integer id) {
+		
+		obj = serv.update(obj);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
+				path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.noContent().build();
+	}	
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> delete(@PathVariable Integer id) {
+		
+		serv.delete(id);
+
+		return ResponseEntity.noContent().build();
+	}	
 
 }
--- server/FinancJ/src/main/java/com/feijo/financj/services/ContaService.java
+++ server/FinancJ/src/main/java/com/feijo/financj/services/ContaService.java
@@ -1,6 +1,6 @@
 package com.feijo.financj.services;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,12 +10,38 @@ import com.feijo.financj.repositories.ContaRepository;
 
 @Service
 public class ContaService {
-	
+
 	@Autowired
 	ContaRepository repo;
-	
+
 	public Conta find(Integer id) {
 		Conta conta = repo.findById(id).orElse(null);
 		return conta;
+	}
+
+	public List<Conta> findAll() {
+		return repo.findAll();
+	}
+
+	public Conta insert(Conta obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Conta update(Conta obj) {
+		Conta newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		repo.deleteById(id);
+	}
+
+	private void updateData(Conta newObj, Conta obj) {
+		newObj.setDescricao(obj.getDescricao());
+		newObj.setNumero(obj.getNumero());
+		newObj.setSaldo(obj.getSaldo());
 	}
 }
