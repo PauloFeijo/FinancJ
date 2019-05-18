7cd337d419d48fc36752150a6f7f84cd5c8abbcb
PauloFeijo <paulorikrdo.89@gmail.com>
2019-05-18 11:21:55 -0300

    Ajustes de criação

--- server/FinancJ/src/main/java/com/feijo/financj/FinancJApplication.java
+++ server/FinancJ/src/main/java/com/feijo/financj/FinancJApplication.java
@@ -1,4 +1,4 @@
-package com.paulofeijo.financj;
+package com.feijo.financj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- server/FinancJ/src/test/java/com/feijo/financj/FinancJApplicationTests.java
+++ server/FinancJ/src/test/java/com/feijo/financj/FinancJApplicationTests.java
@@ -1,4 +1,4 @@
-package com.paulofeijo.financj;
+package com.feijo.financj;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
