720803bbdb7a978f33caec9e0d117d21aa2115e7
PauloFeijo <paulorikrdo.89@gmail.com>
2019-05-18 11:13:32 -0300

    Criação do projeto e Modelagem

---
+++ server/FinancJ/src/main/java/com/paulofeijo/financj/FinancJApplication.java
@@ -0,0 +1,13 @@
+package com.paulofeijo.financj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancJApplication.class, args);
+	}
+
+}
