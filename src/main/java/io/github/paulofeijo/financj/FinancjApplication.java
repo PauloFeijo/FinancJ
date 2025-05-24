a59ffd9141028cf4553bf09560cede84977fb5df
PauloFeijo <paulorikrdo.89@gmail.com>
2025-05-24 12:12:15 -0300

    Create project by start.spring.io

---
+++ src/main/java/io/github/paulofeijo/financj/FinancjApplication.java
@@ -0,0 +1,12 @@
+package io.github.paulofeijo.financj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancjApplication.class, args);
+	}
+}
---
+++ src/test/java/io/github/paulofeijo/financj/FinancjApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.paulofeijo.financj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
